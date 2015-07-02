a8053f97391cb261d3169088002cbc32cebf4f5a
iamsuperbootstrap <iamsuperbootstrap@gmail.com>
2015-07-02 21:48:38 +0530

    created bootstrap browser mocks

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+jQuery(function($) {
+    // CUSTOM LINKS SCROLLING FUNCTION 
+    $('a[href*=#]').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+       && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target
+            || $('[name=' + this.hash.slice(1) + ']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top;
+                $('html,body')
+                .animate({ scrollTop: targetOffset }, 800); //set scroll speed here
+                return false;
+            }
+        }
+    });     
+});
